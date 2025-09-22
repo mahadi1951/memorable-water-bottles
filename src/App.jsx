56c954faee9e74f5678a8e9fd7ca01d9ac8f854e
mahadi1951 <mahadihassan1951@gmail.com>
2025-09-22 23:07:21 +0600

    json file added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Suspense } from 'react';
 import './App.css'
 import Bottles from './components/Bottles/Bottles';
 
-const bottlesPromise = fetch('./Bottles.json').then(res => res.json());
+
+const bottlesPromise2 = fetch('https://raw.githubusercontent.com/mahadi1951/memorable-water-bottles/refs/heads/main/public/Bottles.json').then(res => res.json());
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
       <h1>Buy Awesome water Bottle</h1>
      
      <Suspense fallback={<h3>Bottles are loading....</h3>}>
-    <Bottles bottlesPromise={bottlesPromise}></Bottles>
+    <Bottles bottlesPromise={bottlesPromise2}></Bottles>
      </Suspense>
     </>
   )
